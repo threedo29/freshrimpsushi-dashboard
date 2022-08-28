dd24dc405ca496ffb6dfb28b697bd501d41fee99
SungJae Yang <threedo29@gmail.com>
2022-08-28 23:11:13 +0900

    project clean

--- ajax/_db_config.php
+++
@@ -1,5 +0,0 @@
-<?php
-include_once("../db_config.php");
-$db_database = "dashboard";
-
-?>
--- common.php
+++
@@ -1,5 +0,0 @@
-<?php session_start();
-define("FRESHRIMPSUSHI_URL", "https://freshrimpsushi.com");
-define("DASHBOARD_URL", FRESHRIMPSUSHI_URL."/dashboard");
-define("AJAX_URL", DASHBOARD_URL."/ajax");
-?>
